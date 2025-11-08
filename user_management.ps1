f625b1c2ea375821dad6aaa88a6c467677513b25
aph4406 <aph4406@g.rit.edu>
2025-11-08 18:23:16 -0500

    Add new users to KeepList and ExcludeFromPasswordChange

--- user_management.ps1
+++ user_management.ps1
@@ -1,6 +1,6 @@
 # Save as user_management.ps1
-$KeepList = @("Administrator", "drwho", "martymcfly", "arthurdent", "sambeckett", "loki", "riphunter", "theflash", "tonystark", "drstrange", "bartallen", "whiteteam", "blackteam", "grayteam", "datadog", "dd-dog")
-$ExcludeFromPasswordChange = @("whiteteam", "blackteam", "grayteam", "datadog", "dd-dog", "ddagentuser")
+$KeepList = @("Administrator", "drwho", "martymcfly", "arthurdent", "sambeckett", "loki", "riphunter", "theflash", "tonystark", "drstrange", "bartallen", "whiteteam", "blackteam", "grayteam", "datadog", "dd-dog", "ddagentuser", "dd-agent")
+$ExcludeFromPasswordChange = @("whiteteam", "blackteam", "grayteam", "datadog", "dd-dog", "ddagentuser", "dd-agent")
 $Password = ConvertTo-SecureString "ChangeMeNow!2025" -AsPlainText -Force
 
 # Remove extra users
@@ -13,8 +13,9 @@ Get-LocalUser | Where-Object { $_.Name -notin $KeepList } | ForEach-Object {
 foreach ($user in $KeepList) {
     if ($user -in $ExcludeFromPasswordChange) { continue }
     Set-LocalUser -Name $user -Password $Password
-    Write-Output "Password changed: $user"
+    Write-Output "Password changed: $user
 }
+Admin, Administrator, arthurdent, bartallen, ddagentuser, drstrange, drwho, loki, martymcfly, riphunter, sambeckett, theflash, tonystark, whiteteam
 
 #Single Line
 Get-LocalUser | Where-Object { $_.Name -notin $KeepList } | ForEach-Object { Remove-LocalUser -Name $_.Name; Write-Output "Removed: $($_.Name)" }; $KeepList | Where-Object { $_ -notin $ExcludeFromPasswordChange } | ForEach-Object { Set-LocalUser -Name $_ -Password $Password; Write-Output "Password changed: $_" }
